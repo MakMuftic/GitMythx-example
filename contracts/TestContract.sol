5a890f46a1b5607157fec81878dde62540e206e3
mace <makmuftic@gmail.com>
2019-06-13 15:05:50 +0200

    TestCommit3

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -29,7 +29,7 @@ contract TestContract {
 //    // A dynamically-sized array of `Proposal` structs
 //    Proposal[] public proposals;
 //
-//    /// Create a new ballot to choose one of `proposalNames`.
+//    /// Create a new ballot to choose one of `proposalNames`
 //    constructor(bytes32[] memory proposalNames) public {
 //        chairperson = msg.sender;
 //        voters[chairperson].weight = 1;
