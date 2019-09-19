da24330454509e95f5a6a4e51b8a42503b385016
mace <makmuftic@gmail.com>
2019-09-19 14:49:11 +0200

    Update TestContract.sol

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -4,8 +4,8 @@ pragma solidity 0.5.1;
 contract SimpleStorage {
     uint storedData;
 
-    function set(uint x) public {
-        storedData = x;
+    function set(uint y) public {
+        storedData = y;
     }
 
     function get() public view returns (uint) {
