58ec83c45a6886ea1998455d3856130c972788e9
mace <makmuftic@gmail.com>
2019-09-23 10:51:49 +0200

    Update TestContract.sol

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -1,14 +1,33 @@
-pragma solidity 0.5.1;
+pragma solidity ^0.4.25;
 
-/// Test Contract 7
-contract TestContract {
-    uint storedData;
+contract Wallet {
+    uint[] private bonusCodes;
+    address private owner;
 
-    function set(uint y) public {
-        storedData = y;
+    constructor() public {
+        bonusCodes = new uint[](0);
+        owner = msg.sender;
     }
 
-    function get() public view returns (uint) {
-        return storedData;
+    function () public payable {
+    }
+
+    function PushBonusCode(uint c) public {
+        bonusCodes.push(c);
+    }
+
+    function PopBonusCode() public {
+        require(0 <= bonusCodes.length);
+        bonusCodes.length--;
+    }
+
+    function UpdateBonusCodeAt(uint idx, uint c) public {
+        require(idx < bonusCodes.length);
+        bonusCodes[idx] = c;
+    }
+
+    function Destroy() public {
+        require(msg.sender == owner);
+        selfdestruct(msg.sender);
     }
 }
