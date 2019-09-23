63e541746d9a4e4c4add946b7460484fef73c758
mace <makmuftic@gmail.com>
2019-09-23 11:01:51 +0200

    Update TestContract.sol

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -1,154 +1,153 @@
 pragma solidity 0.5.1;
 
-contract Rubixi {
 
-        //Declare variables for storage critical to contract
-        uint private balance = 0;
-        uint private collectedFees = 0;
-        uint private feePercent = 10;
-        uint private pyramidMultiplier = 300;
-        uint private payoutOrder = 0;
+/// @title Voting with delegation.
+/// Test Bad 44
+contract TestContract {
 
-        address private creator;
+   // This declares a new complex type which will
+   // be used for variables later
+   // It will represent a single voter
+   struct Voter {
+       uint weight; // weight i s accumulated by delegation
+       bool voted;  // if true, that person already voted
+       address delegate; // person delegated to
+       uint vote;   // index of the voted proposal
+   }
 
-        //Sets creator
-        function DynamicPyramid() {
-                creator = msg.sender;
-        }
+   // This is a type for a single proposa
+   struct Proposal {
+       bytes32 name;   // short name (up to 32 bytes)
+       uint voteCount; // number of accumulated votes
+   }
 
-        modifier onlyowner {
-                if (msg.sender == creator) _;
-        }
+   address public chairperson;
 
-        struct Participant {
-                address etherAddress;
-                uint payout;
-        }
+   // This declares a state variable that
+   // stores a `Voter` struct for each possible address.
+   mapping(address => Voter) public voters;
 
-        Participant[] private participants;
+   // A dynamically-sized array of `Proposal` structs
+   Proposal[] public proposals;
 
-        //Fallback function
-        function() {
-                init();
-        }
+   /// Create a new ballot to choose one of `proposalNames`
+   constructor(bytes32[] memory proposalNames) public {
+       chairperson = msg.sender;
+       voters[chairperson].weight = 1;
 
-        //init function run on fallback
-        function init() private {
-                //Ensures only tx with value of 1 ether or greater are processed and added to pyramid
-                if (msg.value < 1 ether) {
-                        collectedFees += msg.value;
-                        return;
-                }
+       // For each of the provided proposal names,
+       // create a new proposal object and add it
+       // to the end of the array.
+       for (uint i = 0; i < proposalNames.length; i++) {
+           // `Proposal({...})` creates a temporary
+           // Proposal object and `proposals.push(...)`
+           // appends it to the end of `proposals`.
+           proposals.push(Proposal({
+               name : proposalNames[i],
+               voteCount : 0
+               }));
+       }
+   }
 
-                uint _fee = feePercent;
-                //50% fee rebate on any ether value of 50 or greater
-                if (msg.value >= 50 ether) _fee /= 2;
+   // Give `voter` the right to vote on this ballot.
+   // May only be called by `chairperson`.
+   function giveRightToVote(address voter) public {
+       // If the first argument of `require` evaluates
+       // to `false`, execution terminates and all
+       // changes to the state and to Ether balances
+       // are reverted.
+       // This used to consume all gas in old EVM versions, but
+       // not anymore.
+       // It is often a good idea to use `require` to check if
+       // functions are called correctly.
+       // As a second argument, you can also provide an
+       // explanation about what went wrong.
+       require(
+           msg.sender == chairperson,
+           "Only chairperson can give right to vote."
+       );
+       require(
+           !voters[voter].voted,
+           "The voter already voted."
+       );
+       require(voters[voter].weight == 0);
+       voters[voter].weight = 1;
+   }
 
-                addPayout(_fee);
-        }
+   /// Delegate your vote to the voter `to`
+   function delegate(address to) public {
+       // assigns reference
+       Voter storage sender = voters[msg.sender];
+       require(!sender.voted, "You already voted.");
 
-        //Function called for valid tx to the contract 
-        function addPayout(uint _fee) private {
-                //Adds new address to participant array
-                participants.push(Participant(msg.sender, (msg.value * pyramidMultiplier) / 100));
+       require(to != msg.sender, "Self-delegation is disallowed.");
 
-                //These statements ensure a quicker payout system to later pyramid entrants, so the pyramid has a longer lifespan
-                if (participants.length == 10) pyramidMultiplier = 200;
-                else if (participants.length == 25) pyramidMultiplier = 150;
+       // Forward the delegation as long as
+       // `to` also delegated.
+       // In general, such loops are very dangerous,
+       // because if they run too long, they might
+       // need more gas than is available in a block.
+       // In this case, the delegation will not be executed,
+       // but in other situations, such loops might
+       // cause a contract to get "stuck" completely.
+       while (voters[to].delegate != address(0)) {
+           to = voters[to].delegate;
 
-                // collect fees and update contract balance
-                balance += (msg.value * (100 - _fee)) / 100;
-                collectedFees += (msg.value * _fee) / 100;
+           // We found a loop in the delegation, not allowed.
+           require(to != msg.sender, "Found loop in delegation.");
+       }
 
-                //Pays earlier participiants if balance sufficient
-                while (balance > participants[payoutOrder].payout) {
-                        uint payoutToSend = participants[payoutOrder].payout;
-                        participants[payoutOrder].etherAddress.send(payoutToSend);
+       // Since `sender` is a reference, this
+       // modifies `voters[msg.sender].voted`
+       sender.voted = true;
+       sender.delegate = to;
+       Voter storage delegate_ = voters[to];
+       if (delegate_.voted) {
+           // If the delegate already voted,
+           // directly add to the number of votes
+           proposals[delegate_.vote].voteCount += sender.weight;
+       } else {
+           // If the delegate did not vote yet,
+           // add to her weight.
+           delegate_.weight += sender.weight;
+       }
+   }
 
-                        balance -= participants[payoutOrder].payout;
-                        payoutOrder += 1;
-                }
-        }
+   /// Give your vote (including votes delegated to you)
+   /// to proposal `proposals[proposal].name`.
+   function vote(uint proposal) public {
+       Voter storage sender = voters[msg.sender];
+       require(sender.weight != 0, "Has no right to vote");
+       require(!sender.voted, "Already voted.");
+       sender.voted = true;
+       sender.vote = proposal;
 
-        //Fee functions for creator
-        function collectAllFees() onlyowner {
-                if (collectedFees == 0) throw;
+       // If `proposal` is out of the range of the array,
+       // this will throw automatically and revert all
+       // changes.
+       proposals[proposal].voteCount += sender.weight;
+   }
 
-                creator.send(collectedFees);
-                collectedFees = 0;
-        }
+   /// @dev Computes the winning proposal taking all
+   /// previous votes into account.
+   function winningProposal() public view
+   returns (uint winningProposal_)
+   {
+       uint winningVoteCount = 0;
+       for (uint p = 0; p < proposals.length; p++) {
+           if (proposals[p].voteCount > winningVoteCount) {
+               winningVoteCount = proposals[p].voteCount;
+               winningProposal_ = p;
+           }
+       }
+   }
 
-        function collectFeesInEther(uint _amt) onlyowner {
-                _amt *= 1 ether;
-                if (_amt > collectedFees) collectAllFees();
-
-                if (collectedFees == 0) throw;
-
-                creator.send(_amt);
-                collectedFees -= _amt;
-        }
-
-        function collectPercentOfFees(uint _pcent) onlyowner {
-                if (collectedFees == 0 || _pcent > 100) throw;
-
-                uint feesToCollect = collectedFees / 100 * _pcent;
-                creator.send(feesToCollect);
-                collectedFees -= feesToCollect;
-        }
-
-        //Functions for changing variables related to the contract
-        function changeOwner(address _owner) onlyowner {
-                creator = _owner;
-        }
-
-        function changeMultiplier(uint _mult) onlyowner {
-                if (_mult > 300 || _mult < 120) throw;
-
-                pyramidMultiplier = _mult;
-        }
-
-        function changeFeePercentage(uint _fee) onlyowner {
-                if (_fee > 10) throw;
-
-                feePercent = _fee;
-        }
-
-        //Functions to provide information to end-user using JSON interface or other interfaces
-        function currentMultiplier() constant returns(uint multiplier, string info) {
-                multiplier = pyramidMultiplier;
-                info = 'This multiplier applies to you as soon as transaction is received, may be lowered to hasten payouts or increased if payouts are fast enough. Due to no float or decimals, multiplier is x100 for a fractional multiplier e.g. 250 is actually a 2.5x multiplier. Capped at 3x max and 1.2x min.';
-        }
-
-        function currentFeePercentage() constant returns(uint fee, string info) {
-                fee = feePercent;
-                info = 'Shown in % form. Fee is halved(50%) for amounts equal or greater than 50 ethers. (Fee may change, but is capped to a maximum of 10%)';
-        }
-
-        function currentPyramidBalanceApproximately() constant returns(uint pyramidBalance, string info) {
-                pyramidBalance = balance / 1 ether;
-                info = 'All balance values are measured in Ethers, note that due to no decimal placing, these values show up as integers only, within the contract itself you will get the exact decimal value you are supposed to';
-        }
-
-        function nextPayoutWhenPyramidBalanceTotalsApproximately() constant returns(uint balancePayout) {
-                balancePayout = participants[payoutOrder].payout / 1 ether;
-        }
-
-        function feesSeperateFromBalanceApproximately() constant returns(uint fees) {
-                fees = collectedFees / 1 ether;
-        }
-
-        function totalParticipants() constant returns(uint count) {
-                count = participants.length;
-        }
-
-        function numberOfParticipantsWaitingForPayout() constant returns(uint count) {
-                count = participants.length - payoutOrder;
-        }
-
-        function participantDetails(uint orderInPyramid) constant returns(address Address, uint Payout) {
-                if (orderInPyramid <= participants.length) {
-                        Address = participants[orderInPyramid].etherAddress;
-                        Payout = participants[orderInPyramid].payout / 1 ether;
-                }
-        }
+   // Calls winningProposal() function to get the index
+   // of the winner contained in the proposals array and then
+   // returns the name of the winner
+   function winnerName() public view
+   returns (bytes32 winnerName_)
+   {
+       winnerName_ = proposals[winningProposal()].name;
+   }
 }
