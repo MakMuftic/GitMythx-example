331bd59c992d7202127d6354dafb5bd57566c4c1
mace <makmuftic@gmail.com>
2019-09-24 13:36:16 +0200

    Update TestContract.sol

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.1;
 
 
 /// @title Voting with delegation.
-/// Test Contract 47
+/// Test Contract 48
 contract TestContract {
 
    // This declares a new complex type which will
